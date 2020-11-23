b935fa88655de50f56d5e36a2e83fcc0393be705
Lani <lanidianatjiam@gmail.com>
2020-11-23 16:21:13 +0700

    all

---
+++ day3/day3.js
@@ -0,0 +1,7 @@
+var a = [1,2,3];
+
+console.log(a.map((val) => val*2)); //return array hasil mapping
+
+console.log(a.forEach((cek) => cek*2)); //ga return apapun jd psti undefined
+
+//forEach lebih untuk jalanin function
